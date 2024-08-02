c9e63983e303db846ee8784e788711ad89c5300c
jhlee <jhlee@dbfis.co.kr>
2024-08-02 17:32:44 +0900

    2024.08.02 금요일 퇴근 전 작업사항 커밋

--- src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebsocketDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebsocketDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketDemoApplication.class, args);
+    }
 }
